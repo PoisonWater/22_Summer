0437a9d9d71fb244a2de82eec77728dc2f96203b
PoisonWater <117010178@link.cuhk.edu.cn>
2021-10-02 23:26:19 -0500

    10/02/2021 doing homework using play.java

--- S206.java
+++ S206.java
@@ -9,14 +9,6 @@
  * }
  */
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 class S206 {
     public ListNode reverseList(ListNode head) {
 
