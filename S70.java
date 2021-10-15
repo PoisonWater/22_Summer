d29597beed50ceac63b1c83d64c4f2ac554ccdd9
PoisonWater <117010178@link.cuhk.edu.cn>
2021-10-14 23:01:43 -0500

    10/14/2021 DP

---
+++ S70.java
@@ -0,0 +1,20 @@
+class S70 {
+
+    private int[] dp;
+    
+    public int climbStairs(int n) {
+        
+        dp = new int[n+1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+
+    }
+
+}
